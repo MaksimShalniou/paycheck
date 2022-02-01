1490aca835e2f8c942281428f7c444e30549839e
Dzmitry-Bohush <dzmitry.bohush@innowise-group.com>
2022-02-01 19:10:38 +0300

    added pre-commit

--- src/shared/hashLinks/ui.tsx
+++ src/shared/hashLinks/ui.tsx
@@ -1,7 +1,13 @@
-import React, { FC, Fragment } from 'react';
-import { HashLink } from 'react-router-hash-link';
-import { HashLinksConfig } from '.';
+import React, { FC, Fragment } from "react";
+import { HashLink } from "react-router-hash-link";
+import { HashLinksConfig } from ".";
 
-export const HashLinks: FC<HashLinksConfig> = ({ config }) => <Fragment>
-    {config.map((value) => <HashLink smooth to={value.path}>{value.title}</HashLink>)}
-</Fragment>
+export const HashLinks: FC<HashLinksConfig> = ({ config }) => (
+  <Fragment>
+    {config.map((value) => (
+      <HashLink key={Math.random()} smooth to={value.path}>
+        {value.title}
+      </HashLink>
+    ))}
+  </Fragment>
+);
