e1797b19463b7258c832449e787ee180e4fc3c47
Dzmitry-Bohush <dzmitry.bohush@innowise-group.com>
2022-04-18 13:02:25 +0300

    feat: added preview files for send

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { WalletService, MetamaskWallet } from "./services/wallet/walletService";
 
+
 export const App = () => {
+  const [filePath, setFilePath] = useState('')
+  const [fileType, setFileType] = useState('')
   const walletService = new WalletService(new MetamaskWallet());
+
+  const changeFile = (e: any) => {
+    setFileType(e.target.files[0].type)
+    const reader = new FileReader();
+
+    reader.onload = (some: any) => {
+      setFilePath(some.target.result)
+    };
+
+    reader.readAsDataURL(e.target.files[0]);
+  }
+  
+  
   return (
     <>
+      <input type='file' onChange={changeFile}/>
+      {fileType.match("image") ? (
+        <img src={filePath} alt='some' />
+      ) : (
+        <video src={filePath} autoPlay muted />
+      )}
       <button onClick={() => walletService.checkIfMetamaskInstalled()}>
         Check if metamask extension installed
       </button>
