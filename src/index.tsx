e0a82bc70f983c12e0a5fcfbf128c31f6985a0e9
Maksim Shalniou <maksim.shalniou@innowise-group.com>
2022-04-25 15:20:24 +0300

    added i18next

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { I18nextProvider } from "react-i18next";
+import i18next, { init } from "i18next";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import commonEn from "./shared/translations/en/common.json";
+
+init({
+  interpolation: { escapeValue: false },
+  lng: "en",
+  resources: {
+    en: {
+      common: commonEn,
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <I18nextProvider i18n={i18next}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </I18nextProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
